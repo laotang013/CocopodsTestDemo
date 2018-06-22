71c06eddf8a720a47b09a5a70091822ab591f02e
Start <250226337@qq.com>
2018-06-22 15:56:05 +0800

    1.0.0

---
+++ CocopodsTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocopodsTestDemo
+//
+//  Created by Start on 2018/6/22.
+//  Copyright © 2018年 Start. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocopodsTestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocopodsTestDemo
+//
+//  Created by Start on 2018/6/22.
+//  Copyright © 2018年 Start. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
